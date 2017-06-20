82908a08f71f803f227c0fadd898985a93d1039a
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2017-06-20 11:00:31 -0300

    Comienzo de subida -4

---
+++ Pokemon/Assets/DragAndThrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndThrow : MonoBehaviour {
+
+    bool dragging = false;
+    float distance;
+    public float ThrowSpeed;
+    public float ArchSpeed;
+    public float Speed;
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        this.GetComponent<Rigidbody>().useGravity = true;
+        this.GetComponent<Rigidbody>().velocity += this.transform.forward*ThrowSpeed;
+        this.GetComponent<Rigidbody>().velocity += this.transform.up * ArchSpeed;
+        dragging = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = Vector3.Lerp(this.transform.position, rayPoint, Speed * Time.deltaTime);
+        }
+		
+	}
+}
